4a711071485ce2fb0b5fef007fb61550669dd7a2
geraldik <geraldik1986@gmail.com>
2021-12-25 14:08:53 +0300

    Применены ранее созданные методы из MatrixCheck.java

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -3,14 +3,8 @@ package ru.job4j.puzzle;
 public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = false;
-        int[] arrayHorizontal = new int[board.length];
-        int[] arrayVertical = new int[board.length];
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                arrayHorizontal[j] = board[i][j];
-                arrayVertical[j] = board[j][i];
-            }
-            if (checkRowLine(arrayHorizontal) || checkRowLine(arrayVertical)) {
+            if (checkHorizontal(board, i) || checkVertical(board, i)) {
                 rsl = true;
                 break;
             }
@@ -18,13 +12,25 @@ public class Win {
         return rsl;
     }
 
-    public static boolean checkRowLine(int[] rowLine) {
-        boolean rsl = true;
-        for (int i = 0; i < rowLine.length; i++) {
-            if (rowLine[i] == 0) {
-                rsl = false;
+    public static boolean checkHorizontal(int[][] board, int row) {
+        boolean result = true;
+        for (int i = 0; i < board[row].length; i++) {
+            if (board[row][i] != 1) {
+                result = false;
+                break;
             }
         }
-        return rsl;
+        return result;
+    }
+
+    public static boolean checkVertical(int[][] board, int column) {
+        boolean result = true;
+        for (int[] ints : board) {
+            if (ints[column] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 }
